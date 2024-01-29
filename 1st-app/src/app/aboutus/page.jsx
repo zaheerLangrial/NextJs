487bd8eb9538c8a83f61694eea446f87e3f7760c
Zaheer Ahmad <zaheerahmadlangrial92@gamil.com>
2024-01-29 19:17:07 +0500

    Learn NextJs

---
+++ 1st-app/src/app/aboutus/page.jsx
@@ -0,0 +1,8 @@
+
+function page() {
+  return (
+    <div className="text-white bg-black h-screen">page</div>
+  )
+}
+
+export default page
---
+++ 1st-app/src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <h1 className=" text-5xl font-bold text-blue-800">Heloo World This is my first App</h1>
+    </main>
+  );
+}
